ee38a26ad6515e362766811c05516231035a518d
Luis Barufi <37280785+luisbarufi@users.noreply.github.com>
2022-07-08 23:54:25 -0300

    Create CreateUserUseCase.spec.ts

---
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -0,0 +1,40 @@
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { CreateUserError } from "./CreateUserError";
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+let inMemoryUsersRepository: IUsersRepository;
+let createUserUseCase: CreateUserUseCase;
+
+describe("Create a User", () => {
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository();
+    createUserUseCase = new CreateUserUseCase(inMemoryUsersRepository);
+  })
+
+  it("should be able to create a new user", async () => {
+    const user = await createUserUseCase.execute({
+      name: "User Name",
+      email: "user@example.com",
+      password : "1234",
+    });
+
+    expect(user).toHaveProperty("id");
+  });
+
+  it('should not be able to create a user with already existing email', async () => {
+    expect(async () => {
+      await createUserUseCase.execute({
+        name: "User Name",
+        email: "user@example.com",
+        password : "1234",
+      });
+
+      await createUserUseCase.execute({
+        name: "User Name2",
+        email: "user@example.com",
+        password : "1234",
+      });
+    }).rejects.toBeInstanceOf(CreateUserError);
+  });
+});
